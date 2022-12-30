a199d53d8b3332d0fc8ab5b88026c55f7e28485c
Bakhmetevanatalia <bakhmeteva@reksoft.ru>
2022-12-30 23:06:49 +0300

    update dz

--- src/test/java/DomTest.java
+++ src/test/java/DomTest.java
@@ -1,90 +1,90 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 import java.util.logging.Logger;
 
 
 
-public class DomTest extends BaseTest{
+public class DomTest {
     private static final Logger log = Logger.getLogger(String.valueOf(DomTest.class));
-    private String login = "wasej93407@dni8.com";
-    private String pas = "Natalia12345!";
+    private final String login = "wasej93407@dni8.com";
+    private final String pas = "Natalia12345!";
+    protected static WebDriver driver;
+    ChromeOptions options = new ChromeOptions();
+
+
+    @BeforeAll
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
 
 
     @Test
     public void headlessMode(){
-    // chooseMode("headless");
-    // Открыть Chrome в headless режиме
-        ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
-    // Перейти на https://duckduckgo.com/
+        driver = new ChromeDriver(options);
         driver.get("https://duckduckgo.com/");
-    // В поисковую строку ввести ОТУС
         driver.findElement(By.id("search_form_input_homepage")).sendKeys("ОТУС", Keys.ENTER);
-    // Проверить что в поисковой выдаче первый результат Онлайн‑курсы для профессионалов, дистанционное обучение
         String element = driver.findElement(By.xpath("//*[@id=\"r1-0\"]/div[2]/h2/a/span")).getText();
         Assertions.assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным ...", element);
-    //  log.info("Ожидаемый результат: Онлайн‑курсы для профессионалов, дистанционное обучение современным ..."+ "\n Фактический результат: " +element);
 
     }
 
-
     @Test
-    public void fullScreenMode() throws InterruptedException {
+    public void fullScreenMode() throws Exception {
         By image = By.xpath("//img[@src='assets/images/p1.jpg']");
-    //  Открыть Chrome в режиме киоска
-        driver.manage().window().fullscreen();
-    //  Перейти на https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818
+        options.addArguments("--kiosk");
+        driver = new ChromeDriver(options);
         driver.get("https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818");
-    //  Нажать на любую картинку
-    //  wait.until(ExpectedConditions.invisibilityOfElementLocated(image));
         JavascriptExecutor js = (JavascriptExecutor)driver;
         js.executeScript("arguments[0].scrollIntoView()", driver.findElement(image));
         js.executeScript("arguments[0].click()", driver.findElement(image));
-    //  Проверить что картинка открылась в модальном окне
-        Thread.sleep(2000);
-        Assertions.assertTrue(driver.findElement(By.xpath("//div[@class='pp_hoverContainer']")).isDisplayed());
-    }
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[@class='pp_hoverContainer']"))));
 
+    }
 
     @Test
     public void maximizeTest(){
-    //  Открыть https://otus.ru
-        driver.quit();
-        driver = new ChromeDriver();
-    //  Не киоск
         driver.manage().window().maximize();
         driver.get("https://otus.ru");
-    //  Авторизоваться на сайте
         loginInOtus();
-    //  Вывести в лог все cookie
         log.info(driver.manage().getCookies().toString()) ;
     }
 
     private void loginInOtus(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.findElement(By.cssSelector(".header3__button-sign-in")).click();
         try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
+            wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".header3__button-sign-in"))));
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        clearAndEnter(By.xpath("//*[@class='new-log-reg__form js-login']/descendant::*[@placeholder='Электронная почта']"), login);
-        clearAndEnter(By.xpath("//*[@class='new-log-reg__form js-login']/descendant::*[@placeholder='Введите пароль']"), pas);
-        driver.findElement(By.cssSelector("div.new-input-line_last:nth-child(5) > button:nth-child(1)")).submit();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+        clearAndEnter(By.xpath("//input[@type='text' and contains (@placeholder, 'Электронная почта')]"), login);
+        clearAndEnter(By.xpath("//input[@type='password' and contains (@placeholder, 'Введите пароль')]"), pas);
+        driver.findElement(By.xpath("//*[normalize-space(text()) = 'Войти' and @type='submit']")).submit();
+     }
 
     private void clearAndEnter(By by, String text){
         driver.findElement(by).clear();
         driver.findElement(by).sendKeys(text);
+    }
+    @AfterEach
+    public void close(){
+        if (driver != null)
+            driver.quit();
     }
 
 }
